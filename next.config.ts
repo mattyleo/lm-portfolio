af06264752385f5e9e8f3eb7190df159eb733378
Mattia Leoni <mattialeoni@gmail.com>
2026-05-01 11:06:02 +0200

    Set output to 'standalone' in next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -1,8 +1,7 @@
 import type { NextConfig } from "next";
-
 const nextConfig: NextConfig = {
+  output: 'standalone',
   images: { unoptimized: true },
   allowedDevOrigins: ['192.168.0.150'],
 };
-
 export default nextConfig;
